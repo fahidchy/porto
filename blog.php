65887440a8a4c7e0f139a2ba99f6bca366d90d96
rey <enarddpascua@gmail.com>
2023-01-24 17:52:52 +1100

    add visitor search feature

--- blog.php
+++ blog.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/styles.css">
-  <link rel="stylesheet" type="text/css" href="css/blog.css">
+  <link rel="stylesheet" type="text/css" href="css/blog.css?v=<?php echo time(); ?>">
   <script src="./jscripts/index.js" defer></script>
   <script src="https://kit.fontawesome.com/692b5d8ceb.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.6.3.slim.min.js" integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
@@ -29,17 +29,41 @@
       ?>
     </div>
   </header>
+  <div id="search-form-wrapper">
+    <form action="" method="post">
+        <input placeholder="Search post here.." type="text" name="search-input"/>
+        <button type="submit" name="search-button">Search</button>
+    </form>
+  </div>
   <main>
     <?php
     include "./php/DatabaseConnection.php";
     $connection = new DatabaseConnection("localhost");
     $pdo = $connection-> connect();
-    $result = $pdo->query("SELECT *, FIRST_NAME, LAST_NAME  FROM post, user WHERE post.USER_ID = user.USER_ID AND post.PUBLISHED = 1")->fetchAll(PDO::FETCH_ASSOC);
+    $postQuery = "SELECT *, FIRST_NAME, LAST_NAME  FROM post, user WHERE post.USER_ID = user.USER_ID AND post.PUBLISHED = 1";
+    $result = $pdo->query($postQuery)->fetchAll(PDO::FETCH_ASSOC);
+
+    if(isset($_POST['search-input'])){
+      $searchInput=strtolower($_POST['search-input']);
+      $query='';
+      if($searchInput !=''){
+        $query = $pdo->prepare("SELECT *, FIRST_NAME, LAST_NAME  FROM post, user WHERE (post.TITLE = ? OR post.DATE_CREATED = ? OR user.FIRST_NAME = ? OR user.LAST_NAME =?) AND post.USER_ID = user.USER_ID AND post.PUBLISHED = 1");
+        $query->execute([$searchInput,$searchInput,$searchInput,$searchInput]);
+      }else{
+        $query = $pdo->query($postQuery);
+      }
+      $result = $query->fetchAll(PDO::FETCH_ASSOC);
+      if(!$result){
+        echo "There's no post match to your query.";
+      }
+    }
+
       
     for($i = 0; $i < count($result); $i++){
         $title = $result[$i]["TITLE"];
         $content= $result[$i]["CONTENT"];
         $image = $result[$i]["IMAGE"];
+        $dateCreated = $result[$i]["DATE_CREATED"];
         $published = $result[$i]["PUBLISHED"];
         $postId = $result[$i]["POST_ID"];
         $name = $result[$i]["FIRST_NAME"]. " " . $result[$i]["LAST_NAME"];
@@ -64,6 +88,7 @@
         <h2><?=$title?></h2>
         <p><?=$content?></p>
         <p>By <?=$name?></p>
+        <p>Date created: <?=$dateCreated?></p>
       </div>
       <form action="" method="post" onsubmit="return">
         <button type="submit" id="like-button" name="like-post-button-<?=$i?>"><i class="fa-solid fa-thumbs-up"></i></button>
