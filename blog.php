22303d99c1feb783201264e6b15fe757d9065741
rey <enarddpascua@gmail.com>
2023-01-25 22:31:13 +1100

    create popular section

--- blog.php
+++ blog.php
@@ -1,3 +1,14 @@
+<?php
+  include "./php/DatabaseConnection.php";
+  $connection = new DatabaseConnection("localhost");
+  $pdo = $connection-> connect();
+
+  //fetch popular posts
+  $popularPostQuery = "SELECT *, FIRST_NAME, LAST_NAME FROM post, user WHERE post.USER_ID = user.USER_ID AND post.PUBLISHED = 1 AND post.POST_LIKE > 1 ORDER BY post.POST_LIKE DESC LIMIT 3";
+  $popularResult = $pdo->query($popularPostQuery)->fetchAll(PDO::FETCH_ASSOC);
+
+  $popularPostId='';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,12 +46,72 @@
         <button type="submit" name="search-button">Search</button>
     </form>
   </div>
-  <main id="blog-main">
+  <section id ="popular-section-wrapper">
+      <h2 class="section-title"><?=count($popularResult)>1 ? "Popular":""?></h2>
     <?php
-    include "./php/DatabaseConnection.php";
-    $connection = new DatabaseConnection("localhost");
-    $pdo = $connection-> connect();
-    $postQuery = "SELECT *, FIRST_NAME, LAST_NAME  FROM post, user WHERE post.USER_ID = user.USER_ID AND post.PUBLISHED = 1";
+     
+      if(count($popularResult) > 1){
+        for($i = 0; $i < count($popularResult); $i++){
+          $title = $popularResult[$i]["TITLE"];
+          $content= $popularResult[$i]["CONTENT"];
+          $image = $popularResult[$i]["IMAGE"];
+          $dateCreated = $popularResult[$i]["DATE_CREATED"];
+          $published = $popularResult[$i]["PUBLISHED"];
+          $postId = $popularResult[$i]["POST_ID"];
+          $name = $popularResult[$i]["FIRST_NAME"]. " " . $popularResult[$i]["LAST_NAME"];
+          $postLike = $popularResult[$i]["POST_LIKE"];
+
+           //like function
+        if(isset($_POST['like-popular-button-' .$i])){
+          $totalLike = $postLike + 1;
+          $query = $pdo->prepare("UPDATE post SET POST_LIKE=? WHERE POST_ID = $postId");
+          $likeResult = $query->execute([$totalLike]);
+          if($likeResult){
+            echo "Success";
+          }else{
+            echo "Failed to like the post";
+          }
+          sleep(5);
+        }
+
+        if(isset($_POST['view-popular-post-' .$i])){
+          session_start();
+          $_SESSION['viewTitle'] = $title;
+          $_SESSION['viewContent'] =$content;
+          $_SESSION['viewAuthor'] = $name;
+          $_SESSION['viewDate'] = $dateCreated;
+          $_SESSION['viewImage']= $image;
+          header("location: php/visitor-view-post.php");
+        }
+      
+    ?>
+      <div class='post-parent-wrapper'>
+          <img alt='post_img' src="<?=substr($image,3)?>"/>
+          <h2><?=$title?></h2>
+          <p><?=$content?></p>
+          <p>By <?=$name?></p>
+          <p>Date created: <?php
+          $dateTime = DateTime::createFromFormat('Y-m-d', $dateCreated);
+          echo date_format($dateTime,"d-M-Y");
+          ?></p>
+          <form action="" method="post" onsubmit="return">
+            <button type="submit" id="like-button" name="like-popular-button-<?=$i?>"><i class="fa-solid fa-thumbs-up"></i></button>
+            <span><?=$postLike?></span>
+            <button type="submit" class ="view-post-button" name="view-popular-post-<?=$i?>">View</button>
+          </form>
+        </div>
+    <?php
+        }
+      }
+
+    ?>
+      
+  </section>
+  <hr>
+  <main id="blog-main">
+  <h2 class="section-title">Blog posts</h2>
+    <?php
+    $postQuery = "SELECT *, FIRST_NAME, LAST_NAME  FROM post, user WHERE post.USER_ID = user.USER_ID AND post.PUBLISHED = 1 ORDER BY post.POST_LIKE DESC";
     $result = $pdo->query($postQuery)->fetchAll(PDO::FETCH_ASSOC);
 
     if(isset($_POST['search-input'])){
@@ -59,7 +130,7 @@
     }
 
       
-    for($i = 0; $i < count($result); $i++){
+    for($i = count($popularResult); $i < count($result); $i++){
         $title = $result[$i]["TITLE"];
         $content= $result[$i]["CONTENT"];
         $image = $result[$i]["IMAGE"];
@@ -104,6 +175,7 @@
         ?></p>
         <form action="" method="post" onsubmit="return">
           <button type="submit" id="like-button" name="like-post-button-<?=$i?>"><i class="fa-solid fa-thumbs-up"></i></button>
+          <span><?=$postLike?></span>
           <button type="submit" class ="view-post-button" name="view-post-<?=$i?>">View</button>
         </form>
       </div>
